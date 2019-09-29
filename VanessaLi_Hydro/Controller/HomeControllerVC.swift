2cc79e6ad894755af66ba649c64f91e2071e1ba1
vanessali <li.vanessaa@gmail.com>
2019-09-29 10:27:19 -0400

    first commit

---
+++ VanessaLi_Hydro/Controller/HomeControllerVC.swift
@@ -0,0 +1,137 @@
+//
+//  ViewController.swift
+//  VanessaLi_Hydro
+//
+//  Created by Vanessa Li on 2019-09-28.
+//  Copyright © 2019 Vanessa Li. All rights reserved.
+//
+
+import UIKit
+
+class HomeControllerVC: UIViewController, UITextFieldDelegate {
+    
+    // Global Variables
+    var onPeak: String = ""
+    var midPeak: String = ""
+    var offPeak : String = ""
+    var value1 : Double = 0.0
+    var value2 : Double = 0.0
+    var value3 : Double = 0.0
+    
+    //Outlets
+    @IBOutlet weak var emailTextInput: UITextField!
+    @IBOutlet weak var phoneTextInput: UITextField!
+    
+    @IBOutlet weak var onPeakInput: UITextField!
+    @IBOutlet weak var midPeakInput: UITextField!
+    @IBOutlet weak var offPeakInput: UITextField!
+    
+ 
+    // this function is the function that is loaded when our application first starts
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // delegate allows us this class to implement built in methods
+        phoneTextInput.delegate = self
+        emailTextInput.delegate = self
+        onPeakInput.delegate = self
+        midPeakInput.delegate = self
+        offPeakInput.delegate = self
+        
+    }
+    
+   //allows keyboard removal when user clicks on enter - using default keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        emailTextInput.resignFirstResponder()
+        return true  //method returns true -otherwise false
+    }
+
+    //Method implemented to toggle NUMBER PAD Keyboard - when user touches outside of txtfield keyboard will go away -- touches textfield to re-appear
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        phoneTextInput.endEditing(true)
+        onPeakInput.endEditing(true)
+        midPeakInput.endEditing(true)
+        offPeakInput.endEditing(true)
+    }
+
+    
+    
+    //Action functions - Calculate Button
+    @IBAction func calculateButton(_ sender: UIButton) {
+        validatingInput()
+        
+        // manually resetting textfields to empty string
+        onPeakInput.text = ""
+        midPeakInput.text = ""
+        offPeakInput.text = ""
+        phoneTextInput.text = ""
+        emailTextInput.text = "" 
+    }
+    
+    
+    // function that checks if user has entered values
+     func validatingInput(){
+        // giving our global variables some default values
+        onPeak = onPeakInput.text ?? ""
+        midPeak = midPeakInput.text ?? ""
+        offPeak = offPeakInput.text ?? ""
+        
+        //checking if text fields are empty
+        if onPeakInput.text?.count != 0 && midPeakInput.text?.count != 0 && offPeakInput.text?.count != 0{
+            calculateHydroAmount()
+        } else { //If user has left peak values empty then call the alertbox function
+           showAlertBox(attempt: false)
+        }
+    }
+    
+    //Calculating stuff
+    func calculateHydroAmount(){
+        //formatting doubles to 2 decimal places
+        value1 =  ((Double(onPeak)! * 0.132) * 100).rounded()/100
+        value2 =  ((Double(midPeak)! * 0.094) * 100).rounded()/100
+        value3 =  ((Double(offPeak)! * 0.065) * 100).rounded()/100
+        
+
+        //Check to see if values are greater than 0 before moving on to next screen
+        if value1 > 0 && value2 > 0 && value3 > 0 {
+            navigateToNextStoryBoard()
+        }
+        
+        
+    }
+    
+    //Navigate to Next Story board that Displays the data
+    func navigateToNextStoryBoard(){
+        let nextStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+        let totalVC = nextStoryBoard.instantiateViewController(withIdentifier: "TotalAmountID") as! TotalAmountVC
+        // passing our  global values from user input to next screen ( TotalVC) via assignment
+        totalVC.value1 = self.value1
+        totalVC.value2 = self.value2
+        totalVC.value3 = self.value3
+        totalVC.onPeak = self.onPeak
+        totalVC.offPeak = self.offPeak
+        totalVC.midPeak = self.midPeak
+     
+        //push to next screen called totalVC
+        navigationController?.pushViewController(totalVC, animated: true)
+    }
+    
+    
+    //Function to create alert boxes when user input is empty
+    func showAlertBox(attempt: Bool){
+        //assigned empty value to string
+        var msg: String = ""
+        if attempt{ // if attempt returns true - it will say ..
+            msg = "this works"
+        } else {// if attempt returns false -- it will say
+            msg = "Try Again"
+        }
+        //the message that pops up when error occurs
+        let alertController = UIAlertController(title: "You can not leave peak values empty", message: msg, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil)) //add action to cancel alert box
+        self.present(alertController, animated: true, completion: nil) // this makes the alert box show up when funcfion is called
+    }
+
+    
+}// end bracket
+
---
+++ VanessaLi_Hydro/Controller/TotalAmountVC.swift
@@ -0,0 +1,94 @@
+//
+//  TotalAmountVC.swift
+//  VanessaLi_Hydro
+//
+//  Created by Vanessa Li on 2019-09-28.
+//  Copyright © 2019 Vanessa Li. All rights reserved.
+//
+
+import UIKit
+
+class TotalAmountVC: UIViewController {
+    //Global Variables
+    var value1 : Double = 0.0
+    var value2 : Double = 0.0
+    var value3 : Double = 0.0
+    var onPeak : String = ""
+    var midPeak : String = ""
+    var offPeak : String = ""
+    var regulatoryCharge : Double = 3.28
+    var totalCharge : Double = 0.0
+    var HST : Double = 0.13
+    var hstAmount : Double = 0.0
+    var provincialRebate: Double = 0.08
+    var rebateAmount : Double = 0.0
+    var totalBillAmount : Double = 0.0
+    
+    
+    
+    //IBOUTLETS
+    @IBOutlet weak var onPeakDollarValue: UILabel!
+    @IBOutlet weak var midPeakDollarValue: UILabel!
+    @IBOutlet weak var offPeakDollarValue: UILabel!
+    
+    @IBOutlet weak var regulatoryChargeLabel: UILabel!
+    @IBOutlet weak var totalChargesLabel: UILabel!
+    @IBOutlet weak var hstChargeLabel: UILabel!
+    @IBOutlet weak var rebateLabel: UILabel!
+    @IBOutlet weak var totalFinalAmountLabel: UILabel!
+    
+    @IBOutlet weak var peakKWHLabel: UILabel!
+    @IBOutlet weak var midKWHLabel: UILabel!
+    @IBOutlet weak var offKWHLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //on load display the values we are retrieving from HomeControllerVC
+        onPeakDollarValue.text = "$" + String(self.value1)
+        midPeakDollarValue.text = "$" + String(self.value2)
+        offPeakDollarValue.text = "$" + String(self.value3)
+        regulatoryChargeLabel.text = "$" + String(self.regulatoryCharge)
+        peakKWHLabel.text = "\(onPeak) kWh @ $0.132/kwh"
+        midKWHLabel.text = "\(midPeak) kWh @ $0.094/kwh"
+        offKWHLabel.text = "\(offPeak) kWh @ $0.065/kwh"
+
+        
+        //Values that needed calculations were made into functions and called when screen loads
+        totalChargesCalculation()
+        HSTCalculation()
+        rebateCalculation()
+        finalAmount()
+        
+    }
+    
+    //Function to calculate total charge = on/mid/off peak + regulatory
+    func totalChargesCalculation(){
+        totalCharge = value1 + value2 + value3 + regulatoryCharge
+        totalChargesLabel.text = "$\(totalCharge)"
+    }
+    
+    //Function for HST - HST * total charges
+    func HSTCalculation(){
+        hstAmount = HST * totalCharge
+        hstChargeLabel.text = "$\((hstAmount * 100).rounded()/100)"
+        
+    }
+    
+    //Provincial rebates is 8% of total charges
+    func rebateCalculation(){
+        rebateAmount = provincialRebate * totalCharge
+        rebateLabel.text = "$\((rebateAmount * 100).rounded()/100)"
+        
+    }
+    
+    //Final Calculation Client must pay is totalcharges + HST - provincial
+    func finalAmount(){
+        let finalHydro = totalCharge + hstAmount - rebateAmount
+        totalFinalAmountLabel.text = "$\((finalHydro * 100).rounded()/100)"
+    }
+    
+    
+   
+
+}// end bracket
